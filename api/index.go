8cac631058bd55065d67d91ed4c1781435e8d3bc
yann <pizixi125@gmail.com>
2023-08-28 18:33:20 +0800

    第一次

---
+++ api/index.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+var srv http.Handler
+
+func init() {
+	proxyURL := os.Getenv("Proxy_URL")
+	target, _ := url.Parse(proxyURL)
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	e := echo.New()
+	e.Any("/*", func(c echo.Context) error {
+		proxy.ServeHTTP(c.Response().Writer, c.Request())
+		return nil
+	})
+	srv = e
+}
+
+func Ф(w http.ResponseWriter, r *http.Request) {
+	srv.ServeHTTP(w, r)
+}
