3a6d18f13dc7d8b60a860b77970c03f414950e51
yann <pizixi125@gmail.com>
2023-09-11 18:00:34 +0800

    还是改回echo

--- api/index.go
+++ api/index.go
@@ -6,55 +6,24 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
 )
 
-// var srv *gin.Engine // 注意这里改为 *gin.Engine
+var srv http.Handler
 
-// func init() {
-// 	proxyURL := os.Getenv("Proxy_URL")
-// 	target, _ := url.Parse(proxyURL)
-// 	proxy := httputil.NewSingleHostReverseProxy(target)
-
-// 	r := gin.Default()
-// 	r.Any("/*any", func(c *gin.Context) {
-// 		proxy.ServeHTTP(c.Writer, c.Request)
-// 	})
-// 	srv = r
-// }
-
-// func Proxy(w http.ResponseWriter, r *http.Request) {
-// 	srv.ServeHTTP(w, r)
-// }
-
-var (
-	app *gin.Engine
-)
-
-// CREATE ENDPOIND
-
-func myRoute(r *gin.RouterGroup) {
-	// r.GET("/admin", func(c *gin.Context) {
-	// 	c.String(http.StatusOK, "Hello from golang in vercel")
-	// })
-
+func init() {
 	proxyURL := os.Getenv("Proxy_URL")
 	target, _ := url.Parse(proxyURL)
 	proxy := httputil.NewSingleHostReverseProxy(target)
-	r.Any("/*any", func(c *gin.Context) {
-		proxy.ServeHTTP(c.Writer, c.Request)
+
+	e := echo.New()
+	e.Any("/*", func(c echo.Context) error {
+		proxy.ServeHTTP(c.Response().Writer, c.Request())
+		return nil
 	})
+	srv = e
 }
 
-func init() {
-	app = gin.New()
-	// r := app.Group("/api")
-	r := app.Group("/")
-	myRoute(r)
-
-}
-
-// Handler ADD THIS SCRIPT
-func Handler(w http.ResponseWriter, r *http.Request) {
-	app.ServeHTTP(w, r)
+func Proxy(w http.ResponseWriter, r *http.Request) {
+	srv.ServeHTTP(w, r)
 }
