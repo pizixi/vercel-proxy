618e99a8f1cf5216a7b30ac2d0298a1debda1418
yann <pizixi125@gmail.com>
2023-09-20 15:53:53 +0800

    host

--- api/index.go
+++ api/index.go
@@ -15,20 +15,11 @@ func init() {
 	proxyURL := os.Getenv("Proxy_URL")
 	target, _ := url.Parse(proxyURL)
 	proxy := httputil.NewSingleHostReverseProxy(target)
-	// 修改代理响应
-	proxy.ModifyResponse = func(res *http.Response) error {
-		// 添加CORS响应头
-		res.Header.Set("Access-Control-Allow-Origin", "*")
-		res.Header.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		res.Header.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Requested-With")
-		res.Header.Set("Content-Type", "text/event-stream")
-		res.Header.Set("Cache-Control", "no-cache")
-		res.Header.Set("Connection", "keep-alive")
-		return nil
-	}
 
 	e := echo.New()
 	e.Any("/*", func(c echo.Context) error {
+		// 设置请求的Host头
+		c.Request().Host = target.Host
 		proxy.ServeHTTP(c.Response().Writer, c.Request())
 		return nil
 	})
