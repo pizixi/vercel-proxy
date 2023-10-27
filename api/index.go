c62b561688bc6bf81f8e86b90fa5d8fb9237a144
pizixi <1547770200@qq.com>
2023-10-27 17:59:55 +0800

    2

--- api/index.go
+++ api/index.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,12 +15,23 @@ var srv http.Handler
 
 func init() {
 	proxyURL := os.Getenv("Proxy_URL")
+
 	target, _ := url.Parse(proxyURL)
 	proxy := httputil.NewSingleHostReverseProxy(target)
 
-	// 设置超时时间
-	proxy.Transport = &http.Transport{
-		ResponseHeaderTimeout: 50,
+	// 设置流式输出和超时时间
+	proxy.FlushInterval = time.Millisecond * 100 // 流式输出的间隔，这里设置为100毫秒，可以根据需求调整
+	proxy.Transport = &http.Transport{           // 设置超时时间
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   60 * time.Second, // 超时时间，从环境变量中读取
+			KeepAlive: 60 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	}
 
 	e := echo.New()
